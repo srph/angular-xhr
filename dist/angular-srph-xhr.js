39c28782b3b033f7e9913d165b269442c05b51ff
srph <seraphipod@gmail.com>
2014-11-24 23:34:45 +0800

    (updated) build files
    (updated) bower && changelog

--- dist/angular-srph-xhr.js
+++ dist/angular-srph-xhr.js
@@ -192,7 +192,7 @@
       // Append base URL is if not an aboslute url.
       // Return omitted trailing slashes
       if ( !isAbsoluteURL(url) ) { 
-        url = this.baseURL + '/' + url;
+        url = _this.baseURL + '/' + url;
       }
 
       return processURL(url);
